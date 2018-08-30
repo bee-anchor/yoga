72a92c3628f1d24b4b7872e0e7322a6fef6327fa
bianca linscott <bianca.linscott@infinityworks.com>
2018-08-30 15:54:23 +0100

    doc fix

--- faf/waitables.py
+++ faf/waitables.py
@@ -9,10 +9,9 @@ import logging
 logger = logging.getLogger(__name__)
 
 class successful_click(object):
-    """ An expectation for checking that an element is present on the DOM
-    of a page. This does not necessarily mean that the element is visible.
-    locator - used to find the element
-    returns the WebElement once it is located
+    """ An expectation for checking that a click to an element was successful.
+    element - locator used to find the element
+    returns if the click was successful
     """
 
     def __init__(self, element):
