b55518fc16361a697712334198cf6d401759565f
bianca linscott <bianca.linscott@infinityworks.com>
2018-08-30 15:53:02 +0100

    updated example test runners

--- faf/resources/run_behave.py
+++ faf/resources/run_behave.py
@@ -3,14 +3,16 @@ import os
 from behave.__main__ import run_behave
 from behave.configuration import Configuration
 from faf.driver import set_driver
-from faf.config import get_config
-from faf.args import behave_parser
+from faf.config import setup_config
+from faf.args import behave_parser, arg_validation
 
 if __name__ == '__main__':
 
     args = behave_parser.parse_args()
-    # config = get_config(args)
-    set_driver(get_config())
+    arg_validation(args)
+    setup_config(args)
+    if args.execution != 'non-ui':
+        set_driver(args)
     os.chdir(os.path.join(os.curdir, 'behave'))
     run_behave(Configuration())
 
