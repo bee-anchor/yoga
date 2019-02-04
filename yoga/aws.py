1621a9713cf845d88b0496472c4283becd6fb44b
bianca linscott <bianca.linscott@infinityworks.com>
2019-02-04 17:49:28 +0000

    adding aws functions

--- yoga/aws.py
+++ yoga/aws.py
@@ -1,9 +1,12 @@
 import boto3
 
 
-def upload_screenshot_to_s3(filename, file, bucket_name, s3_access_key=None, s3_secret_access_key=None):
-    if s3_access_key and s3_secret_access_key:
-        s3 = boto3.resource('s3', aws_access_key_id=s3_access_key, aws_secret_access_key=s3_secret_access_key)
+def upload_screenshot_to_s3(filename, file, bucket_name, aws_access_key=None,
+                            aws_secret_access_key=None, aws_session_token=None):
+    if aws_access_key and aws_secret_access_key and aws_session_token:
+        s3 = boto3.resource('s3', aws_access_key_id=aws_access_key,
+                            aws_secret_access_key=aws_secret_access_key,
+                            aws_session_token=aws_session_token)
     else:
         s3 = boto3.resource('s3')
 
@@ -11,3 +14,25 @@ def upload_screenshot_to_s3(filename, file, bucket_name, s3_access_key=None, s3_
     url = f'{s3.meta.client.meta.endpoint_url}/{bucket_name}/{filename}'
     print(url)
     return url
+
+def get_s3_file_contents_as_string(bucket_name, filename, aws_access_key=None,
+                                   aws_secret_access_key=None, aws_session_token=None):
+    if aws_access_key and aws_secret_access_key and aws_session_token:
+        s3 = boto3.resource('s3', aws_access_key_id=aws_access_key,
+                            aws_secret_access_key=aws_secret_access_key,
+                            aws_session_token=aws_session_token)
+    else:
+        s3 = boto3.resource('s3')
+
+    obj = s3.Object(bucket_name, filename)
+    return obj.get()['Body'].read().decode('utf-8')
+
+def get_secure_ssm_parameter(name, aws_access_key=None, aws_secret_access_key=None,
+                             aws_session_token=None):
+    if aws_access_key and aws_secret_access_key and aws_session_token:
+        ssm = boto3.client('ssm', aws_access_key_id=aws_access_key,
+                            aws_secret_access_key=aws_secret_access_key,
+                            aws_session_token=aws_session_token)
+    else:
+        ssm = boto3.client('ssm')
+    return ssm.get_parameter(Name=name, WithDecryption=True)['Parameter']['Value']
