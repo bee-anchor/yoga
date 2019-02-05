d4789ea7f4cffe2ba1d8dea67df670d9c0f33aef
bianca linscott <bianca.linscott@infinityworks.com>
2019-02-05 14:14:19 +0000

    adding aws region to aws functions

--- yoga/aws.py
+++ yoga/aws.py
@@ -2,11 +2,11 @@ import boto3
 
 
 def upload_screenshot_to_s3(filename, file, bucket_name, aws_access_key=None,
-                            aws_secret_access_key=None, aws_session_token=None):
+                            aws_secret_access_key=None, aws_session_token=None, region_name=None):
     if aws_access_key and aws_secret_access_key and aws_session_token:
         s3 = boto3.resource('s3', aws_access_key_id=aws_access_key,
                             aws_secret_access_key=aws_secret_access_key,
-                            aws_session_token=aws_session_token)
+                            aws_session_token=aws_session_token, region_name=region_name)
     else:
         s3 = boto3.resource('s3')
 
@@ -16,11 +16,11 @@ def upload_screenshot_to_s3(filename, file, bucket_name, aws_access_key=None,
     return url
 
 def get_s3_file_contents_as_string(bucket_name, filename, aws_access_key=None,
-                                   aws_secret_access_key=None, aws_session_token=None):
+                                   aws_secret_access_key=None, aws_session_token=None, region_name=None):
     if aws_access_key and aws_secret_access_key and aws_session_token:
         s3 = boto3.resource('s3', aws_access_key_id=aws_access_key,
                             aws_secret_access_key=aws_secret_access_key,
-                            aws_session_token=aws_session_token)
+                            aws_session_token=aws_session_token, region_name=region_name)
     else:
         s3 = boto3.resource('s3')
 
@@ -28,11 +28,11 @@ def get_s3_file_contents_as_string(bucket_name, filename, aws_access_key=None,
     return obj.get()['Body'].read().decode('utf-8')
 
 def get_secure_ssm_parameter(name, aws_access_key=None, aws_secret_access_key=None,
-                             aws_session_token=None):
+                             aws_session_token=None, region_name=None):
     if aws_access_key and aws_secret_access_key and aws_session_token:
         ssm = boto3.client('ssm', aws_access_key_id=aws_access_key,
                             aws_secret_access_key=aws_secret_access_key,
-                            aws_session_token=aws_session_token)
+                            aws_session_token=aws_session_token, region_name=region_name)
     else:
         ssm = boto3.client('ssm')
     return ssm.get_parameter(Name=name, WithDecryption=True)['Parameter']['Value']
