594ebe3578c11b2ddb575a4236bf337d61b621b2
bianca linscott <bianca.linscott@infinityworks.com>
2019-02-05 14:14:19 +0000

    adding aws region to aws functions

--- yoga/aws.py
+++ yoga/aws.py
@@ -2,13 +2,10 @@ import boto3
 
 
 def upload_screenshot_to_s3(filename, file, bucket_name, aws_access_key=None,
-                            aws_secret_access_key=None, aws_session_token=None):
-    if aws_access_key and aws_secret_access_key and aws_session_token:
-        s3 = boto3.resource('s3', aws_access_key_id=aws_access_key,
+                            aws_secret_access_key=None, aws_session_token=None, region_name=None):
+    s3 = boto3.resource('s3', aws_access_key_id=aws_access_key,
                             aws_secret_access_key=aws_secret_access_key,
-                            aws_session_token=aws_session_token)
-    else:
-        s3 = boto3.resource('s3')
+                            aws_session_token=aws_session_token, region_name=region_name)
 
     s3.Bucket(bucket_name).put_object(Key=filename, Body=file, ContentType='image/png', ACL='private')
     url = f'{s3.meta.client.meta.endpoint_url}/{bucket_name}/{filename}'
@@ -16,23 +13,17 @@ def upload_screenshot_to_s3(filename, file, bucket_name, aws_access_key=None,
     return url
 
 def get_s3_file_contents_as_string(bucket_name, filename, aws_access_key=None,
-                                   aws_secret_access_key=None, aws_session_token=None):
-    if aws_access_key and aws_secret_access_key and aws_session_token:
-        s3 = boto3.resource('s3', aws_access_key_id=aws_access_key,
+                                   aws_secret_access_key=None, aws_session_token=None, region_name=None):
+    s3 = boto3.resource('s3', aws_access_key_id=aws_access_key,
                             aws_secret_access_key=aws_secret_access_key,
-                            aws_session_token=aws_session_token)
-    else:
-        s3 = boto3.resource('s3')
+                            aws_session_token=aws_session_token, region_name=region_name)
 
     obj = s3.Object(bucket_name, filename)
     return obj.get()['Body'].read().decode('utf-8')
 
 def get_secure_ssm_parameter(name, aws_access_key=None, aws_secret_access_key=None,
-                             aws_session_token=None):
-    if aws_access_key and aws_secret_access_key and aws_session_token:
-        ssm = boto3.client('ssm', aws_access_key_id=aws_access_key,
+                             aws_session_token=None, region_name=None):
+    ssm = boto3.client('ssm', aws_access_key_id=aws_access_key,
                             aws_secret_access_key=aws_secret_access_key,
-                            aws_session_token=aws_session_token)
-    else:
-        ssm = boto3.client('ssm')
+                            aws_session_token=aws_session_token, region_name=region_name)
     return ssm.get_parameter(Name=name, WithDecryption=True)['Parameter']['Value']
