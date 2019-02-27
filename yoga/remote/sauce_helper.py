670469ef0b48d87832902230b6c399566bf9a7ca
bianca linscott <bianca.linscott@infinityworks.com>
2019-02-27 16:09:51 +0000

    Switching to using requests for sauce api interactions, issues using sauceclient with new data center

--- yoga/remote/sauce_helper.py
+++ yoga/remote/sauce_helper.py
@@ -1,15 +1,27 @@
-from sauceclient import SauceClient
+import requests
+from requests.auth import HTTPBasicAuth
 from yoga.context import CONTEXT
 
 
 class SauceHelper:
 
     def __init__(self):
-        self.sauce_client = SauceClient(CONTEXT.config['remote_service']['username'], CONTEXT.config['remote_service']['access_key'])
+        self.rest_api = CONTEXT.config.get('remote_service', 'api_url', fallback="https://saucelabs.com/rest")
+        self.username = CONTEXT.config.get('remote_service', 'username')
+        self.access_key = CONTEXT.config.get('remote_service', 'access_key')
+        self.headers = {'Content-Type': 'application/json'}
+
+    def auth(self):
+        """Create auth string from credentials."""
+        return HTTPBasicAuth(self.username, self.access_key)
 
     def report_outcome(self, session_id, test_passed):
-        self.sauce_client.jobs.update_job(session_id, passed=test_passed)
+        url = self.rest_api + f"/v1/{self.username}/jobs/{session_id}"
+        body = {"passed": test_passed}
+        requests.put(url, json=body, headers=self.headers, auth=self.auth())
 
     def update_job_name(self, session_id, name):
-        self.sauce_client.jobs.update_job(session_id, name=name)
+        url = self.rest_api + f"/v1/{self.username}/jobs/{session_id}"
+        body = {"name": name}
+        requests.put(url, json=body, headers=self.headers, auth=self.auth())
 
