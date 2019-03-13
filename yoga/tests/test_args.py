a667c1414d900615fcae693987a1a3b97c451c29
Andy Crow <andy.crow@infinityworks.com>
2019-03-13 15:08:11 +0000

    Add appium_remote_real to available execution args

--- yoga/tests/test_args.py
+++ yoga/tests/test_args.py
@@ -19,23 +19,28 @@ def test_arg_validation_fails_for_grid_local_and_no_browser():
 def test_arg_validation_fails_for_selenium_remote_and_no_capabilities():
     args = Namespace(execution='selenium_remote', capability=None)
     with pytest.raises(RuntimeError,
-                       match='When using an execution type of selenium_remote, appium_local or appium_remote, capability must be specified'):
+                       match='When using an execution type of selenium_remote, appium_local, appium_remote or appium_remote_real, capability must be specified'):
         arg_validation(args)
 
 
 def test_arg_validation_fails_for_appium_local_and_no_capabilities():
     args = Namespace(execution='appium_local', capability=None)
     with pytest.raises(RuntimeError,
-                       match='When using an execution type of selenium_remote, appium_local or appium_remote, capability must be specified'):
+                       match='When using an execution type of selenium_remote, appium_local, appium_remote or appium_remote_real, capability must be specified'):
         arg_validation(args)
 
 
 def test_arg_validation_fails_for_appium_remote_and_no_capabilities():
     args = Namespace(execution='appium_remote', capability=None)
     with pytest.raises(RuntimeError,
-                       match='When using an execution type of selenium_remote, appium_local or appium_remote, capability must be specified'):
+                       match='When using an execution type of selenium_remote, appium_local, appium_remote or appium_remote_real, capability must be specified'):
         arg_validation(args)
 
+def test_arg_validation_fails_for_appium_remote_real_and_no_capabilities():
+    args = Namespace(execution='appium_remote_real', capability=None)
+    with pytest.raises(RuntimeError,
+                       match='When using an execution type of selenium_remote, appium_local, appium_remote or appium_remote_real, capability must be specified'):
+        arg_validation(args)
 
 def test_pytest_args_generates_maximum_possible_args():
     args = Namespace(test_dir='test/dir', keyword_expression='keyword', mark_expression='mark and mark1', debug=True)
