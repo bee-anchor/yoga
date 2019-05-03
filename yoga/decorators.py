93dcc8dbeec6df0896a6a24b7c3f3a38b5985132
bianca linscott <bianca.linscott@infinityworks.com>
2019-05-03 15:21:28 +0100

    Added default arg to control decorator printing

--- yoga/decorators.py
+++ yoga/decorators.py
@@ -5,9 +5,10 @@ from colorama import Fore
 def scenario(description):
     def decorator_test(func):
         @functools.wraps(func)
-        def wrapper_test(*args, **kwargs):
-            print(Fore.CYAN + '\n-----------------------------------------------------')
-            print(Fore.LIGHTCYAN_EX + description + Fore.RESET)
+        def wrapper_test(*args, desc_print=True, **kwargs):
+            if desc_print:
+                print(Fore.CYAN + '\n-----------------------------------------------------')
+                print(Fore.LIGHTCYAN_EX + description + Fore.RESET)
             return func(*args, **kwargs)
         return wrapper_test
     return decorator_test
@@ -16,8 +17,9 @@ def scenario(description):
 def step(description):
     def decorator_step(func):
         @functools.wraps(func)
-        def wrapper_step(*args, **kwargs):
-            print(Fore.LIGHTMAGENTA_EX + description.format(*args) + Fore.RESET)
+        def wrapper_step(*args, desc_print=True, **kwargs):
+            if desc_print:
+                print(Fore.LIGHTMAGENTA_EX + description.format(*args) + Fore.RESET)
             return func(*args, **kwargs)
         return wrapper_step
     return decorator_step
